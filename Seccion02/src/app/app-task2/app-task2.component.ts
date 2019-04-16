8f71e32b9861d19201ea1a6a988ebb3c894d3f49
Matias Gomez <jmgametal@gmail.com>
2019-04-16 14:17:09 -0300

    Clase

--- Seccion02/src/app/app-task2/app-task2.component.ts
+++ Seccion02/src/app/app-task2/app-task2.component.ts
@@ -16,7 +16,8 @@ export class AppTask2Component implements OnInit {
 
   onTogleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
 }
